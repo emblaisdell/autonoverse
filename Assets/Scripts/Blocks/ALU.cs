2f6d44512c74691d9f6afb169e87fa125b8afda0
Eben Blaisdell <emb038@bucknell.edu>
2018-02-01 18:58:41 -0500

    Processing block improvements

--- Assets/Scripts/Blocks/ALU.cs
+++ Assets/Scripts/Blocks/ALU.cs
@@ -4,17 +4,113 @@ using UnityEngine;
 
 public class ALU : Block {
 
+	byte input1 = 0;
+	byte input2 = 0;
+	byte opCode = 0;
+	byte output = 0;
+
+	bool upToDate = false;
+
 	override public int memoryMapSize {
 		get {
-			return 0;
+			return 4;
 		}
 	}
 
-	override public byte Read(int offset) {
-		return 0;
+	void Calculate() {
+		switch (opCode) {
+		case 0:
+			output = (byte)(input1 + input2);
+			return;
+		case 1:
+			output = (byte)(input1 - input2);
+			return;
+		case 2:
+			output = (byte)(input1 * input2);
+			return;
+		case 3:
+			if (input2 == 0) {
+				output = byte.MaxValue;
+			} else {
+				output = (byte)(input1 / input2);
+			}
+			return;
+		case 4:
+			output = (byte)(input1 % input2);
+			return;
+		/*case 5: //exponentiation
+			return;*/
+		case 6:
+			output = (byte)(-input1);
+			return;
+		case 16:
+			output = (byte)(input1 & input2);
+			return;
+		case 17:
+			output = (byte)(input1 | input2);
+			return;
+		case 18:
+			output = (byte)(input1 ^ input2);
+			return;
+		case 19:
+			output = (byte)(~input1);
+			return;
+		case 32:
+			output = Bool (input1 == input2);
+			return;
+		case 33:
+			output = Bool (input1 != input2);
+			return;
+		case 34:
+			output = Bool (input1 < input2);
+			return;
+		case 35:
+			output = Bool (input1 <= input2);
+			return;
+		case 36:
+			output = Bool (input1 > input2);
+			return;
+		case 37:
+			output = Bool (input1 >= input2);
+			return;
+		default:
+			output = 0;
+			return;
+		}
 	}
 
-	override public void Write(int offset, byte value){}
+	static byte Bool (bool b) {
+		return b ? byte.MaxValue : byte.MinValue;
+	}
+
+	override public byte Read(int offset) {
+		if (offset == 0) {
+			return input1;
+		} else if (offset == 1) {
+			return input2;
+		} else if (offset == 2) {
+			return opCode;
+		} else {
+			if (!upToDate) {
+				Calculate ();
+				upToDate = true;
+			}
+			return output;
+		}
+	}
+
+	override public void Write(int offset, byte value){
+		if (offset < 3) {
+			if (offset == 0) {
+				input1 = value;
+			} else if (offset == 1) {
+				input2 = value;
+			} else if (offset == 2) {
+				opCode = value;
+			}
+			upToDate = false;
+		}
+	}
 
 	override public void Interact (char c) {}
 
--- Assets/Scripts/Blocks/Tesseract.cs
+++ Assets/Scripts/Blocks/Tesseract.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
 public class Tesseract : Block {
 
 	const int fileLength = 128;
-	byte[] filenameBytes = new byte[fileLength];
+	byte[] filenameBytes = new byte[fileLength+1]; // last byte always '\0'
 	byte filenameBytesPos = 0;
 
 	string contents = "";
@@ -13,28 +15,63 @@ public class Tesseract : Block {
 
 	bool newFile = false;
 
+	const byte EOF = 0x1a;
+
+	byte value;
+
 	override public int memoryMapSize {
 		get {
 			return 4;
 		}
 	}
 
+	string GetPath() {
+		return Encoding.ASCII.GetString (filenameBytes);
+	}
+	void LoadFile() {
+		StreamReader reader = new StreamReader (GetPath (), Encoding.ASCII);
+		contents = reader.ReadToEnd ();
+	}
+	byte GetValue() {
+		if (contentPos >= contents.Length) {
+			return EOF;
+		}
+		return (byte)(contents [contentPos]);
+	}
+
 	override public byte Read(int offset) {
-		return 0;
-		/*if (offset == 0) {
-			return filenameBytes[filenameBytesPos];
+		if (offset == 0) {
+			byte output = filenameBytes[filenameBytesPos];
+			filenameBytesPos++;
+			return output;
 		} else if (offset == 1) {
 			return filenameBytesPos;
 		} else if (offset == 2) {
 			if (newFile) {
-				TextAsset
+				LoadFile ();
+				newFile = false;
 			}
+			byte output = GetValue();
+			contentPos++;
+			return output;
 		} else {
-
-		}*/
+			return contentPos;
+		}
 	}
 
-	override public void Write(int offset, byte value){}
+	override public void Write(int offset, byte value){
+		if (offset == 0) {
+			filenameBytes [filenameBytesPos] = value;
+			filenameBytesPos++;
+			if (filenameBytesPos >= fileLength) {
+				filenameBytesPos = 0;
+			}
+		} else if (offset == 1) {
+			filenameBytesPos = (byte)(value % fileLength);
+		} else if (offset == 3) {
+			contentPos = value;
+		}
+	}
 
 	override public void Interact (char c) {}
 
